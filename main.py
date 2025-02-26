ede3222b448097577c2aa4724f910f169f13dd70
crazysong2022 <smartwayimport@outlook.com>
2025-02-27 01:08:36 +0800

    password type changed

--- main.py
+++ main.py
@@ -57,8 +57,7 @@ def login(username, password):
             cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
             result = cursor.fetchone()
             if result:
-                 # 修改此处，将字符串转换为字节类型
-                hashed_password = result[0].encode('utf-8')
+                hashed_password = bytes(result[0])
                 if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
                     st.session_state['authentication_status'] = True
                     st.session_state['user'] = username
--- models/project_models.py
+++ models/project_models.py
@@ -10,7 +10,7 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    password = Column(LargeBinary, nullable=False)
     
     nursing_topics = relationship("NursingTopic", back_populates="user")
     my_goals = relationship("MyGoals", back_populates="user")
